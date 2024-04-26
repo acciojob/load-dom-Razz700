8215e5ff46f85840c974de1c74df6e38d024df48
Razz700 <160166041+Razz700@users.noreply.github.com>
2024-04-26 17:11:14 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 //your JS code here. If required.
 document.addEventListener("DOMContentLoaded",()=>{
-	let a=document.createElement("p");
-	a.textContent="DOM load success";
-	document.querySelector("body").appendChild(a);
+	console.log("DOM load success");
+	//alert("DOM load success");
+	
+	//let a=document.createElement("p");
+	//a.textContent="DOM load success";
+	//document.querySelector("body").appendChild(a);
 })
