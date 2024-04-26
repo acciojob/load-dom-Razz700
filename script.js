fe616d775d4c48dfeac84d56aa1f0da48f64015f
Razz700 <160166041+Razz700@users.noreply.github.com>
2024-04-26 17:14:07 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 //your JS code here. If required.
 document.addEventListener("DOMContentLoaded",()=>{
 	console.log("DOM load success");
-	//alert("DOM load success");
+	alert("DOM load success");
 	
 	//let a=document.createElement("p");
 	//a.textContent="DOM load success";
 	//document.querySelector("body").appendChild(a);
-})
+});
